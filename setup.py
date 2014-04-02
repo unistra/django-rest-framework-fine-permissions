fc8ccd0d2546486f5bd878752a3a0acd93074d70
Arnaud Grausem <arnaud.grausem@unistra.fr>
2014-04-02 14:04:40 +0200

    new minor version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name = 'djangorestframework-fine-permissions',
-    version = '0.2.0',
+    version = '0.2.1',
     packages = find_packages(),
     install_requires = libraries,
     dependency_links = dependency_links,
