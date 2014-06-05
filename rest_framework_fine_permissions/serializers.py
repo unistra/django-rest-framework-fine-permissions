d1907eaf7dc2399d290593fff93e02aba429b88e
Arnaud Grausem <arnaud.grausem@unistra.fr>
2014-06-05 13:58:35 +0200

    options for defining nested relations when depth is set

--- rest_framework_fine_permissions/serializers.py
+++ rest_framework_fine_permissions/serializers.py
@@ -3,15 +3,24 @@
 """
 """
 
-from rest_framework.serializers import ModelSerializer
-from rest_framework.serializers import RelatedField
-
+from rest_framework import serializers
 from .models import FieldPermission
 
 
-class ModelPermissionsSerializer(ModelSerializer):
+class NestedModelPermissionsSerializerOptions(serializers.ModelSerializerOptions):
     """
     """
+
+    def __init__(self, meta):
+        super(NestedModelPermissionsSerializerOptions, self).__init__(meta)
+        self.nested_fields_default = getattr(meta, 'nested_fields_default')
+
+
+class ModelPermissionsSerializer(serializers.ModelSerializer):
+    """
+    """
+
+    _options_class = NestedModelPermissionsSerializerOptions
 
     def __init__(self, *args, **kwargs):
         super(ModelPermissionsSerializer, self).__init__(*args, **kwargs)
@@ -42,11 +51,16 @@ class ModelPermissionsSerializer(ModelSerializer):
             class Meta:
                 model = related_model
                 depth = self.opts.depth - 1
+                nested_fields_default = self.opts.nested_fields_default
 
         serializer = NestedModelPermissionsSerializer(many=to_many,
             context=self.context)
 
         if not serializer.fields:
-            return RelatedField(many=to_many)
+            for field_name, rel_type in self.opts.nested_fields_default:
+                if field_name == model_field.name:
+                    return rel_type(many=to_many)
+
+            return serializers.RelatedField(many=to_many)
 
         return serializer
