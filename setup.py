3652e0b60c1ccd4904944c6b20a5dd203b8d73c2
Morgan <morgan.bohn@unistra.fr>
2014-06-30 16:49:02 +0200

    add version 0.2.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name = 'djangorestframework-fine-permissions',
-    version = '0.2.2',
+    version = '0.2.3',
     packages = find_packages(),
     install_requires = libraries,
     dependency_links = dependency_links,
