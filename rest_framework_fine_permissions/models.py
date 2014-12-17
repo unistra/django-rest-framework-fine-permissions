c98251cd3c329b8bcb5c49852c56d95b72e599e3
Morgan <morgan.bohn@unistra.fr>
2014-12-17 14:02:50 +0100

    add unique together constraint

--- rest_framework_fine_permissions/models.py
+++ rest_framework_fine_permissions/models.py
@@ -47,6 +47,7 @@ class FilterPermissionModel(models.Model):
         verbose_name = _('user filter permission')
         verbose_name_plural = _('user filters permissions')
         db_table = 'drf_user_filter_permissions'
+        unique_together = ('user', 'content_type',)
 
     def __unicode__(self):
         return '{0.content_type.app_label} | {0.content_type.model} | {0.user.username} | {0.filter}'\
