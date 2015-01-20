0fdfb9479442fbc7ccf61a7e8c8b82bc7d42d389
Arnaud Grausem <arnaud.grausem@unistra.fr>
2015-01-20 11:32:23 +0100

    add permissions for head requests

--- rest_framework_fine_permissions/permissions.py
+++ rest_framework_fine_permissions/permissions.py
@@ -25,7 +25,7 @@ class FullDjangoModelPermissions(DjangoModelPermissions):
     perms_map = {
         'GET': ['%(app_label)s.view_%(model_name)s'],
         'OPTIONS': [],
-        'HEAD': [],
+        'HEAD': ['%(app_label)s.view_%(model_name)s'],
         'POST': ['%(app_label)s.add_%(model_name)s'],
         'PUT': ['%(app_label)s.change_%(model_name)s'],
         'PATCH': ['%(app_label)s.change_%(model_name)s'],
