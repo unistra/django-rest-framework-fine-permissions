75a04ececbfce564e90057c1da9c6a7bdcd6b139
tschlecht <thierry.schlecht@gmail.com>
2022-07-21 12:03:38 +0200

    Add Django AppConfigs

---
+++ rest_framework_fine_permissions/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class FinePermissionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'rest_framework_fine_permissions'
+    verbose_name = 'Rest Framework Fine Permissions'
---
+++ tests/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TestConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'tests'
