d3bc18f4f8f8cf42f150aabf02f44f72a282d087
tschlecht <thierry.schlecht@gmail.com>
2023-07-18 14:15:53 +0200

    Version 0.9.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='djangorestframework-fine-permissions',
-    version='0.9.1',
+    version='0.9.2',
     packages=find_packages(exclude=["tests*"]),
     install_requires=libraries,
     dependency_links=dependency_links,
